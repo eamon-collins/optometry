de436e695533c9141626b50f86555b62c707294f
eamon-collins <eamonacollins@gmail.com>
2017-06-01 17:39:32 -0400

    competes

--- optometry/forms.py
+++ optometry/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ImageForm(forms.Form):
 	imgurl = forms.CharField(max_length=250)
 	image = forms.ImageField()
-	compares = forms.MultipleChoiceField(choices=['Google', 'IBM', 'Amazon'])
+	competitors = forms.MultipleChoiceField(choices=['Google', 'IBM', 'Amazon'])
