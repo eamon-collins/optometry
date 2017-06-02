940fcb0864b741240cf6c19d898676f0115fc947
ec3bd <ec3bd@virginia.edu>
2017-06-02 01:54:44 -0400

    this bs

--- optometry/views.py
+++ optometry/views.py
@@ -1,14 +1,18 @@
 import requests
-from forms import ImageForm
+from .forms import ImageForm
+from django.shortcuts import render
 
 def index(request):
-	if request.method == 'GET':
-		return render('index.html')
+    if request.method == 'GET':
+        form = ImageForm()
+        return render('index.html', {'form': form})
 
 
 def compare(request):
-	if request.method == 'POST':
-		url = request.GET.get('imgurl', None)
-			if url:
-				data = ImageForm(request.POST)
-				
+    if request.method == 'POST':
+        url = request.GET.get('imgurl', None)
+        if url:
+            data = ImageForm(request.POST)
+            if data.imgurl:
+                return
+
