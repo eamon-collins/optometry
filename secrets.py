1a2b30ade4ccdcb821fa1ee89d8701b1e0c0831e
eamon-collins <eamonacollins@gmail.com>
2017-06-06 13:57:29 -0400

    removing secrets from public

--- secrets.py
+++
@@ -1,9 +0,0 @@
-#Secrets and keys here, never serve to client
-GOOGLE_API_KEY = 'AIzaSyBSi3vfV0Udq6pIGp0YHiAgv75G5J_WwB0'
-
-IBM_API_KEY = '10d9a6f1088e7d03be24a6025f5aef3c3387c6f2'
-
-AMAZON_API_KEY_ID = 'AKIAI3RNFWHIQRPCAKPA'
-AMAZON_API_KEY = 'zI/k7E9psInnvw+ArcIwujf8BsaUC8Vwe/izhdrs'
-
-MICROSOFT_API_KEY = '8e1d03498c844d02ad9149e449cbff0e'
